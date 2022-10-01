536701571e2a857be2614a781a9870df78815820
ReenaH <Reena_Hunjan@yahoo.com>
2022-10-01 16:07:02 -0400

    final

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
 
-import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
+import ThoughtList from "../components/ThoughtList";
+import FriendList from "../components/FriendList";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import Auth from '../utils/auth';
+import { useQuery } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
@@ -39,7 +39,7 @@ const Profile = (props) => {
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
       </div>
 
